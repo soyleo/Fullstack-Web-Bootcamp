4128a1fd54755d80e0f0018c41ea92147e27da1d
Juan Leopoldo Sanguin Victorica <leopoldo230791@gmail.com>
2026-06-28 12:11:46 -0300

    Section 17

---
+++ JS/DOM Challenge Starting Files/index.js
@@ -0,0 +1,4 @@
+document.querySelector('ul li:nth-child(3)').innerHTML = "Leopoldo";
+document.querySelector('li a').style.color = 'red';
+document.querySelector('button').style.backgroundColor = "yellow";
+document.querySelector('h1').classList.add('huge');
